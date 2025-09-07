5b5338d31da60bf2536c3d46350ddb317e0cd90a
juancastillov <juancastillov@uchile.cl>
2025-09-07 16:24:48 -0300

    language flag

--- docs/translate.js
+++
@@ -1,9 +0,0 @@
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({
-    pageLanguage: 'es', // Idioma original de tu sitio
-    includedLanguages: 'en', // Idiomas a los que quieres traducir
-    layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-    autoDisplay: false,
-    multilanguagePage: true
-  }, 'google_translate_element');
-}
